447ad25259a873ef6f182b277b9eb3f1fd5ab418
Jennifer Puerta <puertajennifer@gmail.com>
2021-03-16 15:53:57 -0400

    Update cerrar_jornada.php

--- cerrar_jornada.php
+++ cerrar_jornada.php
@@ -28,11 +28,24 @@ if ($success && $errorMSG == ""){
      }
 }
 
-function borrar_directorio_carga($dir_subida){
-    $files = glob($dir_subida . '/*'); //obtenemos todos los nombres de los ficheros
-    foreach($files as $file){
-        if(is_file($file))
-        unlink($file); //elimino el fichero
+function borrar_directorio_carga($dir){    
+    $count = 0;
+
+    // ensure that $dir ends with a slash so that we can concatenate it with the filenames directly
+    $dir = rtrim($dir, "/\\") . "/";
+
+    // use dir() to list files
+    $list = dir($dir);
+
+    // store the next file name to $file. if $file is false, that's all -- end the loop.
+    while(($file = $list->read()) !== false) {
+        if($file === "." || $file === "..") continue;
+        if(is_file($dir . $file)) {
+            unlink($dir . $file);
+            $count++;
+        } elseif(is_dir($dir . $file)) {
+            $count += borrar_directorio_carga($dir . $file);
+        }
     }
 }
 
